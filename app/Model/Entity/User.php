65a5a8bc57da4784ed5a7d2b5315ccf0afab11a8
Resende <matheus.resan@gmail.com>
2023-10-12 14:50:37 -0300

    Criando novos usuários administrativos

--- app/Model/Entity/User.php
+++ app/Model/Entity/User.php
@@ -11,16 +11,86 @@ class User
     public $email; 
     public $password;
 
-    public static function getUserByEmail($email)
+
+    /**
+     * Método responsável por cadastrar a instância atual no banco de dados
+     * @return boolean
+     * 
+     * 
+     */
+    public function cadastrar()
     {
-        /**
+        //Insere a instância no banco
+        $this->id = (new Database('usuarios'))->insert([
+            'nome' => $this->nome,
+            'email' => $this->email,
+            'password' => $this->password
+        ]);
+
+        //Sucesso
+        return true;
+    }
+
+     /**
+     * Método responsável por atualizar os dados no banco
+     * @return boolean
+     * 
+     * 
+     */
+    public function atualizar()
+    {
+        return (new Database('usuarios'))->update( 'id = ' . $this->id ,[
+            'nome' => $this->nome,
+            'email' => $this->email,
+            'password' => $this->password
+        ]);
+    }
+
+    /**
+     * Método responsável por excluir os dados no banco
+     * @return boolean
+     * 
+     * 
+     */
+    public function excluir()
+    {
+        return (new Database('usuarios'))->delete( 'id = ' . $this->id);
+    }
+
+    /**
+     * Método responsável por retornar uma instância com base no ID
+     * @param integer $id
+     * @return User
+     *
+     */
+    public static function getUserById($id)
+    {
+        return self::getUsers('id = ' . $id)->fetchObject(self::class);
+    }
+
+
+    /**
          * Método responsável por retornar um  usuário com base em seu e-mail
          * @param string $email
          * @return User 
          * 
          */
-        return (new Database('usuarios'))
-        ->select('email ="' . $email . '"')
-        ->fetchObject(self::class);
+    public static function getUserByEmail($email)
+    {
+        return self::getUsers('email ="' . $email . '"')->fetchObject(self::class);
+    }
+
+    /**
+     * Método responsável por retornar usuários
+     * @param string $where
+     * @param string $order
+     * @param string $limit
+     * @param string $fields
+     * 
+     * @return PDOStatement
+     */
+    public static function getUsers($where = null, $order = null, $limit = null, $fields = '*')
+    {
+        return (new Database('usuarios'))->select($where,$order,$limit,$fields);
     }
 }
